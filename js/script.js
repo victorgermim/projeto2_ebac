13c298b1942e0b123227621e2d5bf77ca048aad6
Victor Germim Prado <152669348+victorgermim@users.noreply.github.com>
2023-12-21 03:41:57 -0300

    projeto2_ebac

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const form = document.getElementById('formularioContato');
+let linhas = '';
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const inputNomeContato= document.getElementById('nome-contato');
+    const inputTelContato= document.getElementById('tel-contato');
+
+    let linha = '<tr>';
+    linha += `<td>${inputNomeContato.value}</td>`;
+    linha += `<td>${inputTelContato.value}</td>`;
+    linha += '</tr>'
+
+    linhas += linha;
+
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+
+    inputNomeContato.value = '';
+    inputTelContato.value = '';
+})
